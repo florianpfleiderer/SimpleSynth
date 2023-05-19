1a98e9e780bc8a12304ce1e3652c7923bf62e2d4
Robert Ristic <robert@ristic.cc>
2023-05-19 11:17:28 +0200

    Add Stk and test it

--- main.cpp
+++ main.cpp
@@ -8,6 +8,28 @@
 #include "glad/glad.h"
 #include "GLFW/glfw3.h"
 
+#include <SineWave.h>
+#include <RtAudio.h>
+
+using namespace stk;
+// This tick() function handles sample computation only.  It will be
+// called automatically when the system needs a new buffer of audio
+// samples.
+int tick( void *outputBuffer, void *inputBuffer, unsigned int nBufferFrames,
+          double streamTime, RtAudioStreamStatus status, void *dataPointer )
+{
+    /* supress warnings */
+    (void)inputBuffer;
+    (void)streamTime;
+    (void)status;
+
+    SineWave *sine = (SineWave *) dataPointer;
+    StkFloat *samples = (StkFloat *) outputBuffer;
+    for ( unsigned int i=0; i<nBufferFrames; i++ )
+        *samples++ = sine->tick();
+    return 0;
+}
+
 static void glfw_error_callback(int error, const char* description)
 {
     std::cerr << "[Glfw Error] " << error << ": " << description << "\n";
@@ -138,6 +160,46 @@ void shutdown(GLFWwindow* window)
 }
 
 int main() {
+
+    // Set the global sample rate before creating class instances.
+    Stk::setSampleRate( 44100.0 );
+    SineWave sine;
+    RtAudio dac;
+    // Figure out how many bytes in an StkFloat and setup the RtAudio stream.
+    RtAudio::StreamParameters parameters;
+    parameters.deviceId = dac.getDefaultOutputDevice();
+    parameters.nChannels = 1;
+    RtAudioFormat format = ( sizeof(StkFloat) == 8 ) ? RTAUDIO_FLOAT64 : RTAUDIO_FLOAT32;
+    unsigned int bufferFrames = RT_BUFFER_SIZE;
+    try {
+        dac.openStream( &parameters, NULL, format, (unsigned int)Stk::sampleRate(), &bufferFrames, &tick, (void *)&sine );
+    }
+    catch ( RtAudioError &error ) {
+        error.printMessage();
+        goto cleanup;
+    }
+    sine.setFrequency(440.0);
+    try {
+        dac.startStream();
+    }
+    catch ( RtAudioError &error ) {
+        error.printMessage();
+        goto cleanup;
+    }
+    // Block waiting here.
+    char keyhit;
+    std::cout << "\nPlaying ... press <enter> to quit.\n";
+    std::cin.get( keyhit );
+    // Shut down the output stream.
+    try {
+        dac.closeStream();
+    }
+    catch ( RtAudioError &error ) {
+        error.printMessage();
+    }
+    cleanup:
+
+
 
     GLFWwindow* const window = create_window(1280, 720, "Simple Synth");
     ImNodes::CreateContext();
