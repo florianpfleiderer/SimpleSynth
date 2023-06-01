079b2a1fbcf1db60a185f63ab78112c031b3d030
Robert Ristic <robert@ristic.cc>
2023-06-02 00:00:40 +0200

    Update example modules

--- include/modules/Oscillator.h
+++ include/modules/Oscillator.h
@@ -9,6 +9,8 @@
 class Oscillator : public Module {
     int _id_output;
 public:
+    bool tick(stk::StkFloat *buffer, unsigned int nBufferFrames, double streamTime, int output_id) override;
+
     Oscillator();
     void draw() override;
 };
