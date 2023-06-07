ce3cbcd606f0c4434deca689ab787d73ea2e52f3
florianpfleiderer <florian@pfleiderer.at>
2023-06-07 12:09:42 +0200

    renamend test variables

--- tests/node_test.cpp
+++ tests/node_test.cpp
@@ -1,13 +1,40 @@
 #include <catch2/catch_test_macros.hpp>
+#include <catch2/catch_template_test_macros.hpp>
 #include "Stk.h"
 #include "../include/modules/EchoNode.h"
 #include "../include/modules/DelayNode.h"
+#include "../include/modules/SineOscillator.h"
+#include "../include/modules/RectOscillator.h"
+
+TEMPLATE_TEST_CASE( "test module.h functions in modules", "[nodes][template]", 
+                    DelayNode, EchoNode, SineOscillator, RectOscillator) {
+    // call default constructor for each type
+    TestType test_type = TestType();
+
+    SECTION( "test default initialisation") {
+        REQUIRE(test_type.getId() > 0);
+        REQUIRE(test_type.getConnections().size() > 0);
+    }
+
+    SECTION("test tick") {
+        stk::StkFrames frames;
+        frames.resize(10, 1);
+        double streamTime = 0.0;
+        int output_id = 2;
+        REQUIRE(test_type.tick(frames, streamTime, output_id) == true);
+    }
+}
 
 // test member function of delay node
-TEST_CASE("Set Delay Length", "DelayNode") {
+TEST_CASE("Delay Node Test", "DelayNode") {
     DelayNode delayNode = DelayNode();
-    REQUIRE(delayNode.getDelayLength() == 0.0f);
 
+    SECTION("basic tests") {
+        REQUIRE(delayNode.getName() == "Delay");
+        REQUIRE(delayNode.getId() == 1);
+        REQUIRE(delayNode.getDelayLength() == 0.0f);
+        REQUIRE(delayNode.getConnections().size() == 2);
+    }
     SECTION("change delay length") {
         delayNode.setDelayLength(100.0f);
         REQUIRE(delayNode.getDelayLength() == 100.0f);
@@ -19,4 +46,24 @@ TEST_CASE("Set Delay Length", "DelayNode") {
     }
 }
 
+// test member function of echo node
+TEST_CASE("Echo Node Test", "EchoNode") {
+    EchoNode echoNode = EchoNode();
 
+    SECTION("basic tests") {
+        REQUIRE(echoNode.getName() == "Echo");
+        REQUIRE(echoNode.getId() == 1);
+        REQUIRE(echoNode.getConnections().size() == 2);
+    }
+}
+
+// test member function of Sine Oscillator node
+TEST_CASE("SineOscillator Node Test", "SineOscillator") {
+    SineOscillator sineOscillator = SineOscillator();
+
+    SECTION("basic tests") {
+        REQUIRE(sineOscillator.getName() == "SineOscillator");
+        REQUIRE(sineOscillator.getId() == 1);
+        REQUIRE(sineOscillator.getConnections().size() == 1);
+    }
+}
--- tests/oscillator_test.cpp
+++ tests/oscillator_test.cpp
@@ -1,7 +1,14 @@
 #include <catch2/catch_test_macros.hpp>
+#include "Stk.h"
+#include "../include/modules/SineOscillator.h"
+#include "../include/modules/Oscillator.h"
+#include "../include/modules/RectOscillator.h"
 
-// Demonstrate some basic assertions.
-TEST_CASE("OsziTest", "[BasicAssertions]") {
-  // Expect two strings not to be equal.
+TEST_CASE("Sine Oscillator Test", "[SineOscillator]") {
+  SineOscillator sineOscillator = SineOscillator();
+  SECTION("Check default frequency") {
+    REQUIRE(sineOscillator. == 440.0f);
+  }
+
   REQUIRE(2 == 2);
 }
