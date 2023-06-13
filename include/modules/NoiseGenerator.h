46e4e67f2e60e7846c97d634a29ac2ae506dee67
Theresa <e01620751@student.tuwien.ac.at>
2023-06-13 13:22:59 +0200

    add comments to noisegenerator and sweep

--- include/modules/NoiseGenerator.h
+++ include/modules/NoiseGenerator.h
@@ -12,18 +12,33 @@
 class NoiseGenerator : public Module {
 public:
     NoiseGenerator();
-    NoiseGenerator(int id,  int id_output);
+    NoiseGenerator(int id, int id_output);
 
-    //static std::shared_ptr<Module> unserialize(std::stringstream& module_str, int module_id);
-
+    /**
+     * Generate a tick of noise signal
+     *
+     * @param frames StkFrames object to store the generated samples
+     * @param streamTime Current stream time
+     * @param output_id ID of the output connector
+     * @return True if successful, false otherwise
+     */
     bool tick(stk::StkFrames &frames, double streamTime, int output_id) override;
+
+    /**
+     * Draw the NoiseGenerator module node in the GUI
+     */
     void draw() override;
+
+    /**
+     * Serialize the settings of the NoiseGenerator module
+     *
+     * @param ostream Output stream to write the settings to
+     */
     void serialize_settings(std::ostream &ostream) override;
 
 private:
-    stk::Noise noise;
-
-    int _id_output;
+    stk::Noise noise;  ///< TNoise generator object
+    int _id_output;    ///< ID of the output connector
 };
 
 #endif //SIMPLESYNTH_NOISEGENERATOR_H
--- include/modules/Sweep.h
+++ include/modules/Sweep.h
@@ -11,28 +11,57 @@
 #include "../../include/IdGenerator.h"
 #include "Module.h"
 
+/**
+ * @brief Sweep class represents a module that generates a frequency sweep
+ */
 class Sweep : public Module {
 public:
+    /**
+     * @brief Constructor for the Sweep class
+     * @param name Name of the module
+     * @param startFreq Start frequency of the sweep
+     * @param endFreq End frequency of the sweep
+     * @param duration Duration of the sweep in seconds
+     */
     Sweep(std::string name = "Sweep", double startFreq = 500, double endFreq = 10000, float duration = 10);
 
-    //void startSweep();
-    //void stopSweep();
+    /**
+     * @brief Draw the module node in the user interface
+     */
     void draw() override;
+
+    /**
+     * @brief Processe audio frames and generates the frequency sweep
+     * @param frames Audio frames to process
+     * @param streamTime Stream time in seconds
+     * @param output_id ID of the output connector
+     * @return True if the processing is successful, false otherwise
+     */
     bool tick(stk::StkFrames &frames, double streamTime, int output_id) override;
+
+    /**
+     * @brief Update the frequency of the sweep
+     * @param frequency New frequency value
+     */
     void updateFrequency(float frequency);
+
+    /**
+     * @brief Serialize the module settings to an output stream
+     * @param ostream Output stream to write the settings to
+     */
     void serialize_settings(std::ostream &ostream) override;
 
 private:
-    stk::SineWave sineWave;
-    float sweepStartFrequency;
-    float sweepEndFrequency;
-    float sweepDuration;
-    double startTime;
-    bool isSweeping;
-    int _id_output;
-    int _id_start_frequency;
-    int _id_end_frequency;
-    int _id_duration;
+    stk::SineWave sineWave;        /**< Sine wave generator used for the sweep. */
+    float sweepStartFrequency;     /**< Start frequency of the sweep. */
+    float sweepEndFrequency;       /**< End frequency of the sweep. */
+    float sweepDuration;           /**< Duration of the sweep in seconds. */
+    double startTime;              /**< Start time of the sweep in milliseconds. */
+    bool isSweeping;               /**< Flag indicating if the sweep is currently active. */
+    int _id_output;                /**< ID of the output connector. */
+    int _id_start_frequency;       /**< ID of the start frequency attribute. */
+    int _id_end_frequency;         /**< ID of the end frequency attribute. */
+    int _id_duration;              /**< ID of the duration attribute. */
 };
 
 #endif //SIMPLESYNTH_SWEEP_H
