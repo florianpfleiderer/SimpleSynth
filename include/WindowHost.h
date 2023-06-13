c03996187fa81576675854ed2077d15cb9255847
florianpfleiderer <florian@pfleiderer.at>
2023-06-13 14:51:30 +0200

    moved window management to window host

---
+++ include/WindowHost.h
@@ -0,0 +1,25 @@
+//
+// Created by Florian Pfleiderer on 13.05.2023.
+//
+
+#ifndef SIMPLESYNTH_WINDOWHOST_H
+#define SIMPLESYNTH_WINDOWHOST_H
+
+#include <fstream>
+#include <regex>
+#include <memory>
+
+#include "GLFW/glfw3.h"
+#include "backends/imgui_impl_glfw.h"
+#include "backends/imgui_impl_opengl3.h"
+#include <iostream>
+
+class WindowHost {
+public:
+    static void glfw_error_callback(int error, const char* description);
+    static GLFWwindow* create_window(int width, int height, const char* title);
+    // static void end_frame(GLFWwindow* window, ImVec4 background_color);
+    static void shutdown(GLFWwindow* window);
+};
+
+#endif //SIMPLESYNTH_WINDOWHOST_H
